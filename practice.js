2c8f5fa6be51bc42ad9e075fe2c5f7728f71b8a0
Harshitakatara34 <khushikatara041@gmail.com>
2022-11-11 22:43:53 +0530

    comp

---
+++ practice.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("mySidenav").style.height="100px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
